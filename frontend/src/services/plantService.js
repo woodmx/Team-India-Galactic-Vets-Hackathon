0e06af9885efacd7ea311803e00be8bc0c926a41
woodmx <01m.underwood@gmail.com>
2024-05-19 10:34:22 -0700

    final changes to frontend

--- frontend/src/services/plantService.js
+++ frontend/src/services/plantService.js
@@ -1,27 +1,26 @@
-// import axios from "axios";
+import axios from "axios";
 
-// let plantService = {
-//   endpoint: "http://localhost:8000/api/v1/crops/",
-// };
+let plantService = {
+  endpoint: "http://localhost:8000/api/v1/crops/",
+};
 
-// plantService.getCrops = () => {
-//   const config = {
-//     method: "GET",
-//     url: `${plantService.endpoint}`,
-//     // withCredentials: true,
-//     // crossdomain: true,
-//     headers: { "Content-Type": "application/json" }
-//   }
-//   return axios(config);
-// }
-// export default plantService;
+plantService.getCrops = () => {
+  const config = {
+    method: "GET",
+    url: `${plantService.endpoint}`,
+    // withCredentials: true,
+    // crossdomain: true,
+    headers: { "Content-Type": "application/json" }
+  }
+  return axios(config);
+}
+export default plantService;
 
-
-// export async function getAllCrops() {
-//   const response = axios.get('http://localhost:8000/api/v1/crops/', {
-//     headers: {
-//       "Content-Type": "application/json"
-//     }
-//   })
-//   return response
-// }
+export async function getAllCrops() {
+  const response = axios.get('http://localhost:8000/api/v1/crops/', {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+  return response
+}
