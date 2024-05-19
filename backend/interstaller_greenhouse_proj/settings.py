0137b141d4bef49b5400364d15ac168b0fc3fa18
Neka Blair <ourblairfamily@gmail.com>
2024-05-19 08:11:13 -0500

    hide django secret key

--- backend/interstaller_greenhouse_proj/settings.py
+++ backend/interstaller_greenhouse_proj/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-y1j+bl%qg*!qjqpse$_mb*!0t6926q@a!!u)j*jabh35af_k#5'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
